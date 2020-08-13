5ffb04384f4aa76806d7b39d3b6280ed25cb6e32
COUTEYEN, Jean-marie <jean-marie.couteyen-carpaye@airbus.com>
2020-08-13 15:38:55 +0200

    new specification for the yaml, multiple metrics for one validation

--- getting_started/custom_scripts/add_custom_metrics.py
+++ getting_started/custom_scripts/add_custom_metrics.py
@@ -1,8 +1,8 @@
 import gcvb
 
 def main():
-    gcvb.add_metric("absolute",1000)
-    gcvb.add_metric("host_dependent",505)
+    gcvb.add_metric("config_independent_metric",1000)
+    gcvb.add_metric("config_dependent_metric",505)
 
 if __name__ == '__main__':
     main()
